c9d1147c30ba7c5236482325ab9eaf22f91142ad
ptx <ptx@openengine.dk>
2010-08-30 12:26:53 +0000

    First import

---
+++ README.txt
@@ -0,0 +1,10 @@
+Author: OpenEngine Team
+
+Homepage: http://www.openengine.dk/wiki/Projects/MRISIM
+
+Get the latest version with:
+  darcs get http://openengine.dk/code/projects/MRISIM
+
+This is an example project demonstrating how projects work.
+For more infomation on how to create your own projects checkout
+http://www.openengine.dk/wiki/CreatingProjects
---
+++ main.cpp
@@ -0,0 +1,51 @@
+// main
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
+
+// OpenEngine stuff
+#include <Meta/Config.h>
+#include <Logging/Logger.h>
+#include <Logging/StreamLogger.h>
+#include <Core/Engine.h>
+
+// SimpleSetup
+#include <Utils/SimpleSetup.h>
+
+// Game factory
+//#include "GameFactory.h"
+
+// name spaces that we will be using.
+// this combined with the above imports is almost the same as
+// fx. import OpenEngine.Logging.*; in Java.
+using namespace OpenEngine::Logging;
+using namespace OpenEngine::Core;
+using namespace OpenEngine::Utils;
+
+/**
+ * Main method for the first quarter project of CGD.
+ * Corresponds to the
+ *   public static void main(String args[])
+ * method in Java.
+ */
+int main(int argc, char** argv) {
+    // Setup logging facilities.
+    Logger::AddLogger(new StreamLogger(&std::cout));
+
+    // Print usage info.
+    logger.info << "========= Running OpenEngine Test Project =========" << logger.end;
+
+    // Create simple setup
+    SimpleSetup* setup = new SimpleSetup("Example Project Title");
+    // Start the engine.
+    setup->GetEngine().Start();
+
+    // Return when the engine stops.
+    return EXIT_SUCCESS;
+}
+
+
