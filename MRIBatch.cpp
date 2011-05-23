3702a802e45bd41c910bf306c0259466cb74327c
salomonhippie <salomonhippie@gmail.com>
2011-05-23 12:40:25 +0000

    and added the MRIBatch.cpp file ...

---
+++ MRIBatch.cpp
@@ -0,0 +1,19 @@
+#include <Resources/DirectoryManager.h>
+#include <Resources/ResourceManager.h>
+
+
+#include "Resources/Phantom.h"
+#include "Resources/SimplePhantomBuilder.h"
+#include "Resources/Sample3DTexture.h"
+
+#include "Science/SpinEchoSequence.h"
+#include "Science/ExcitationPulseSequence.h"
+#include "Science/TestRFCoil.h"
+
+
+using namespace MRI::Scene;
+using namespace MRI::Science;
+using namespace MRI::Resources;
+
+int main(int argc, char* argv[]) {
+}
